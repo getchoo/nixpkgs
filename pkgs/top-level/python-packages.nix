873ec9dfd98e03e1609574423362a122d5b534a0
seth <getchoo@tuta.io>
2024-11-17 18:09:06 -0500

    python311Packages.pyppmd: init at 1.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -12137,6 +12137,8 @@ self: super: with self; {
 
   pyppeteer = callPackage ../development/python-modules/pyppeteer { };
 
+  pyppmd = callPackage ../development/python-modules/pyppmd { };
+
   pypresence = callPackage ../development/python-modules/pypresence { };
 
   pyprind = callPackage ../development/python-modules/pyprind { };
