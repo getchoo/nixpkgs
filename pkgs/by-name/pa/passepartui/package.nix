cb635b34dee62186bfe6df78df6c6e77a45a2b21
seth <getchoo@tuta.io>
2025-01-14 20:07:35 +0000

    passepartui: 0.1.5 -> 0.1.7
    Diff: https://github.com/kardwen/passepartui/compare/v0.1.5...v0.1.7
    
    Changelog: https://github.com/kardwen/passepartui/releases/tag/v0.1.7

--- pkgs/by-name/pa/passepartui/package.nix
+++ pkgs/by-name/pa/passepartui/package.nix
@@ -1,22 +1,28 @@
 {
   lib,
   fetchFromGitHub,
+  gpgme,
   nix-update-script,
+  pkg-config,
   rustPlatform,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "passepartui";
-  version = "0.1.5";
+  version = "0.1.7";
 
   src = fetchFromGitHub {
     owner = "kardwen";
     repo = "passepartui";
     tag = "v${version}";
-    hash = "sha256-/rYdOurPlpGKCMAXTAhoWRn4NU3usPpJmef3f8V8EQA=";
+    hash = "sha256-LV/2+oSGVBRrWaHP/u1PcCb1T6Nduna/lusakCZW+PM=";
   };
 
-  cargoHash = "sha256-8TmrsRbFC1TyoiUNVVTEdvDLj0BIJmqM00OoPLpaoFQ=";
+  cargoHash = "sha256-SgyVUJ7g0rw+Y6ufpfdbZEZqjLzIZ6lwqlZ+49mn6Ss=";
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ gpgme ];
 
   passthru = {
     updateScript = nix-update-script { };
